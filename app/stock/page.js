9c6d99f23275ee8f70d9aad5a56c88883f56fa06
digiboi-rony <hello.mdronymia@gmail.com>
2026-03-11 18:34:06 +0600

    Update page.js

--- app/stock/page.js
+++ app/stock/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
-import AppShell from '../../components/layout/AppShell';
-import { useAuthStore, useNotifStore } from '../../lib/store';
+import AppShell from '@/components/layout/AppShell';
+import { useAuthStore, useNotifStore } from '@/lib/store';
 
 export default function StockPage() {
   const { token } = useAuthStore();
