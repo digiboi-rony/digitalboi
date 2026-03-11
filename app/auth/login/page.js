304da2f7b07609da4da70befd27819621e0707c2
digiboi-rony <hello.mdronymia@gmail.com>
2026-03-11 17:22:39 +0600

    Update page.js

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAuthStore } from '../../lib/store';
+import { useAuthStore } from '@/lib/store';
 import Link from 'next/link';
 
 export default function LoginPage() {
