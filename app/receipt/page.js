763a0f3df660374827084eb9728a6cbc975c3514
digiboi-rony <hello.mdronymia@gmail.com>
2026-03-11 18:49:32 +0600

    Update page.js

--- app/receipt/page.js
+++ app/receipt/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useState, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
-import { useAuthStore } from '../../lib/store';
+import { useAuthStore } from '@/lib/store';
 
 function ReceiptContent() {
   const params = useSearchParams();
